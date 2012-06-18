9e95ad3e0bdaf1b1725c75ffbd727b4d77e3ef0d
IIEIIEJI <tburnashevr@yandex.ru>
2012-06-19 03:01:38 +0400

    1

---
+++ nodetest.js
@@ -0,0 +1,8 @@
+/**
+ * Created with PyCharm.
+ * User: timurburnashev
+ * Date: 19.06.12
+ * Time: 2:59
+ * To change this template use File | Settings | File Templates.
+ */
+var http = require('http').createServer()
